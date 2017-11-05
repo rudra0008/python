f314b998681fa29534165dd92c5e9196858d7c73
rudra0008 <rnsingh215@gmail.com>
2017-11-05 21:28:01 +0530

    prompt a program

---
+++ prompt.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python -tt
+def main():
+	print"using input()"
+	var1=input("please enter a value:")
+	print"value of var1 is:",var1
+	print"var1 is of type:",type(var1)
+if __name__ == "__main__":
+	main()
