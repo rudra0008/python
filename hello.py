9bfad721326d8d7b0a393877c365d768c936a3ec
rudra0008 <rnsingh215@gmail.com>
2017-11-05 18:25:16 +0530

    hello world program

---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python -tt
+#define the main() function that prints Hello world
+def main():
+	print 'Hello world'
+#standard boiler plate syntax that calls the main() function.
+
+if __name__ == '__main__':
+	main()
+
