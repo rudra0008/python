40bcafd5abae4c8af6cb664dfc43f023b46798dc
rudra0008 <rudra215@gmail.com>
2017-11-08 20:52:43 +0530

    python argparse module

---
+++ parse.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python -tt
+
+# Command:      python argparse.py
+# Description:  python argparse module
+# Author:       Rudra
+# Maintainer:   rudra0008
+# Project:      https://github.com/rudra0008/python
+# Dependencies: 
+import argparse
+parser = argparse.ArgumentParser()
+parser.parse_args()
+
+"""
+OUTPUT:
+
+rudra@rudran:~/python$ python parse.py 
+rudra@rudran:~/python$ python parse.py -h
+usage: parse.py [-h]
+
+optional arguments:
+  -h, --help  show this help message and exit
+
+"""
