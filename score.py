a93de19c8e5f57a993e59d51c9e67de705b86ea2
rudra0008 <rudra215@gmail.com>
2017-12-04 14:31:09 +0530

    list with function

--- score.py
+++ score.py
@@ -7,29 +7,29 @@
 # Dependencies: 
 
 lloyd = {
-          "name": "Lloyd",
-          "homework": [90.0, 97.0, 75.0, 92.0],
-          "quizzes": [88.0, 40.0, 94.0],
-          "tests": [75.0, 90.0]
-                }
+        "name": "Lloyd",
+        "homework": [90.0, 97.0, 75.0, 92.0],
+        "quizzes": [88.0, 40.0, 94.0],
+        "tests": [75.0, 90.0]
+    }
 alice = {
-          "name": "Alice",
-          "homework": [100.0, 92.0, 98.0, 100.0],
-          "quizzes": [82.0, 83.0, 91.0],
-          "tests": [89.0, 97.0]
-                }
+        "name": "Alice",
+        "homework": [100.0, 92.0, 98.0, 100.0],
+        "quizzes": [82.0, 83.0, 91.0],
+        "tests": [89.0, 97.0]
+    }
 tyler = {
-          "name": "Tyler",
-          "homework": [0.0, 87.0, 75.0, 22.0],
-          "quizzes": [0.0, 75.0, 78.0],
-          "tests": [100.0, 100.0]
-                }
+        "name": "Tyler",
+        "homework": [0.0, 87.0, 75.0, 22.0],
+        "quizzes": [0.0, 75.0, 78.0],
+        "tests": [100.0, 100.0]
+    }
 
 # Add your function below!
 def average(numbers):
     total = sum(numbers)
     total = float(total)
-    return total / len(numbers)
+    return total/len(numbers)
 
 def get_average(student):
     homework = average(student["homework"])
@@ -40,21 +40,32 @@ def get_average(student):
 def get_letter_grade(score):
     if score >= 90:
         return "A"
-    elif score >= 80:
+    elif score >=80:
         return "B"
-    elif score >= 70:
+    elif score >=70:
         return "C"
     elif score >=60:
         return "D"
     else:
         return "F"
-                                                  
-print get_average(lloyd)
+
+def get_class_average(class_list):
+    results = []
+    for student in class_list:
+        student_avg = get_average(student)
+        results.append(student_avg)
+    return average(results)
+
+students = [lloyd, alice, tyler]
+class_avg = get_class_average(students)
+print class_avg
+print get_letter_grade(class_avg)
 
 """
 OUTPUT:
 
 rudra@rudran:~/python$ python score.py 
-80.55
+83.8666666667
+B
 
 """
